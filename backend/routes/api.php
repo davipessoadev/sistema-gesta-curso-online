5c6650792112d6ebf5984536697caf0e04957235
Davi Pessoa <davialvespessoa@gmail.com>
2025-09-20 16:27:40 -0300

    Criando os models da aplicação

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/courses', function (Request $request) {
+    return Course::all();
+});
